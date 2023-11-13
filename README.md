f2dee929e907432d7e87ddef5d221f4f0a9d17a9
HahhVwgwg <180103097@stu.sdu.edu.kz>
2023-11-13 13:32:49 +0600

    change js package name

--- README.md
+++ README.md
@@ -21,22 +21,31 @@
 ## Run project
 
 ### Android
+
 To run the application on android device/emulator:
+
 - open project in Android Studio and run imported android run configuration
 
 To build the application bundle:
+
 - run `./gradlew :composeApp:assembleDebug`
 - find `.apk` file in `composeApp/build/outputs/apk/debug/composeApp-debug.apk`
 
 ### Desktop
+
 Run the desktop application: `./gradlew :composeApp:run`
 
 ### iOS
+
 To run the application on iPhone device/simulator:
+
 - Open `iosApp/iosApp.xcworkspace` in Xcode and run standard configuration
-- Or use [Kotlin Multiplatform Mobile plugin](https://plugins.jetbrains.com/plugin/14936-kotlin-multiplatform-mobile) for Android Studio
+- Or
+  use [Kotlin Multiplatform Mobile plugin](https://plugins.jetbrains.com/plugin/14936-kotlin-multiplatform-mobile)
+  for Android Studio
 
 ### Browser
+
 Run the browser application: `./gradlew :composeApp:jsBrowserDevelopmentRun`
 
 ## Especial thanks
@@ -45,14 +54,24 @@ Run the browser application: `./gradlew :composeApp:jsBrowserDevelopmentRun`
 * [Voyager] simply: makes your kmm life easy
 
 [KMM]: https://kotlinlang.org/lp/mobile/
+
 [Flow]: https://github.com/Kotlin/kotlinx.coroutines
+
 [Sqldelight]: https://cashapp.github.io/sqldelight/
+
 [Ktor]: https://ktor.io/
+
 [The Rick and Morty Api]: https://rickandmortyapi.com/
+
 [Alex Fuhrmann]: https://axelfuhrmann.com/
+
 [Koin]: https://insert-koin.io/docs/setup/v3
+
 [Compose]: https://www.jetbrains.com/lp/compose-multiplatform/
+
 [MVI]: https://abhiappm
+
 [Voyager]: https://voyager.adriel.cafe/
+
 [Compose Multiplatform Wizard]: https://terrakok.github.io/Compose-Multiplatform-Wizard/
 
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -26,10 +26,10 @@ kotlin {
 
     jvm("desktop")
 
-//    js {
-//        browser()
-//        binaries.executable()
-//    }
+    js {
+        browser()
+        binaries.executable()
+    }
 
     iosX64()
     iosArm64()
@@ -88,7 +88,6 @@ kotlin {
                 implementation(libs.koin.compose)
                 implementation(libs.sqlDelight.extensions)
                 implementation(libs.resources.compose)
-                implementation(libs.compose.material.dialogs.core)
                 implementation(libs.image.loader.extension.moko.resources)
                 implementation(libs.multiplatform.settings.no.arg)
                 implementation(libs.time.klock)
@@ -132,15 +131,15 @@ kotlin {
             }
         }
 
-//        val jsMain by getting {
-//            dependsOn(commonMain)
-//            dependencies {
-//                implementation(compose.html.core)
-//                implementation(libs.sqlDelight.driver.sqljs)
-//                implementation(npm("sql.js", "1.6.2"))
-//                implementation(devNpm("copy-webpack-plugin", "9.1.0"))
-//            }
-//        }
+        val jsMain by getting {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(compose.html.core)
+                implementation(libs.sqlDelight.driver.sqljs)
+                implementation(npm("sql.js", "1.6.2"))
+                implementation(devNpm("copy-webpack-plugin", "9.1.0"))
+            }
+        }
 
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -208,7 +207,7 @@ tasks.getByPath("desktopSourcesJar").dependsOn("libresGenerateResources")
 dependencies {
     implementation(libs.play.services.wallet)
 }
-//tasks.getByPath("jsProcessResources").dependsOn("libresGenerateResources")
+tasks.getByPath("jsProcessResources").dependsOn("libresGenerateResources")
 
 sqldelight {
     databases {
