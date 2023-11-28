a5f9e7692f594dc5f06ac26ae21440e0fc969667
HahhVwgwg <180103097@stu.sdu.edu.kz>
2023-11-28 13:31:52 +0600

    add composable dna yellow button

---
+++ composeApp/src/commonMain/kotlin/com/dna/payments/kmm/presentation/ui/features/drawer_navigation/DrawerView.kt
@@ -0,0 +1,10 @@
+package com.dna.payments.kmm.presentation.ui.features.drawer_navigation
+
+import androidx.compose.runtime.Composable
+import com.dna.payments.kmm.domain.model.nav_item.NavItem
+
+@Composable
+fun DrawerView(navItems: List<NavItem>) {
+
+}
+
