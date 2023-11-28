44ee884f9cba1344de5ff4542749a516d47a9832
HahhVwgwg <180103097@stu.sdu.edu.kz>
2023-11-28 13:32:13 +0600

    remove imports

--- composeApp/src/commonMain/kotlin/com/dna/payments/kmm/di/UseCaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/dna/payments/kmm/di/UseCaseModule.kt
@@ -1,11 +1,6 @@
 package com.dna.payments.kmm.di
 
-import com.dna.payments.kmm.data.preferences.DefaultPreferences
-import com.dna.payments.kmm.data.preferences.Preferences
 import com.dna.payments.kmm.data.repository.ChangePasswordUseCaseImpl
-import com.dna.payments.kmm.data.repository.DefaultAccessLevelRepository
-import com.dna.payments.kmm.data.repository.DefaultAuthorizationRepository
-import com.dna.payments.kmm.data.repository.DefaultResetPasswordRepository
 import com.dna.payments.kmm.data.repository.SendOtpInstructionsUseCaseImpl
 import com.dna.payments.kmm.data.repository.VerifyOtpCodeUseCaseImpl
 import com.dna.payments.kmm.domain.interactors.use_cases.access_level.AccessLevelUseCase
@@ -16,33 +11,12 @@ import com.dna.payments.kmm.domain.interactors.use_cases.profile.MerchantUseCase
 import com.dna.payments.kmm.domain.interactors.validation.ValidateCode
 import com.dna.payments.kmm.domain.interactors.validation.ValidateEmail
 import com.dna.payments.kmm.domain.interactors.validation.ValidatePassword
-import com.dna.payments.kmm.domain.repository.AccessLevelRepository
-import com.dna.payments.kmm.domain.repository.AuthorizationRepository
 import com.dna.payments.kmm.domain.repository.ChangePasswordUseCase
-import com.dna.payments.kmm.domain.repository.ResetPasswordRepository
 import com.dna.payments.kmm.domain.repository.SendOtpInstructionsUseCase
 import com.dna.payments.kmm.domain.repository.VerifyOtpCodeUseCase
-import com.dna.payments.kmm.presentation.ui.features.drawer_navigation.DrawerNavigationViewModel
-import com.dna.payments.kmm.presentation.ui.features.login.LoginViewModel
-import com.dna.payments.kmm.presentation.ui.features.nav_auth.NavAuthViewModel
-import com.dna.payments.kmm.presentation.ui.features.new_password.NewPasswordViewModel
-import com.dna.payments.kmm.presentation.ui.features.pincode.PinViewModel
-import com.dna.payments.kmm.presentation.ui.features.restore_password.RestorePasswordViewModel
-import com.dna.payments.kmm.presentation.ui.features.verification_code.VerificationCodeViewModel
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.coroutines.Dispatchers
-import kotlinx.serialization.json.Json
-import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
