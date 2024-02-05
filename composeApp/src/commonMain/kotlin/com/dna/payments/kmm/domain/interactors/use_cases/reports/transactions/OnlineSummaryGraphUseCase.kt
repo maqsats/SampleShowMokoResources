eab1aca4bb9e3457f7bb4a3ab5114d4939368822
Maksat Inkar <maksat.inkar.2003@gmail.com>
2024-02-05 13:16:20 +0600

    update libraries

--- composeApp/src/commonMain/kotlin/com/dna/payments/kmm/domain/interactors/use_cases/reports/transactions/OnlineSummaryGraphUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/dna/payments/kmm/domain/interactors/use_cases/reports/transactions/OnlineSummaryGraphUseCase.kt
@@ -3,7 +3,7 @@ package com.dna.payments.kmm.domain.interactors.use_cases.reports.transactions
 import com.dna.payments.kmm.domain.model.date_picker.IntervalType
 import com.dna.payments.kmm.domain.network.Response
 import com.dna.payments.kmm.utils.chart.histogram.HistogramEntry
-import com.soywiz.klock.DateTime
+import korlibs.time.DateTime
 
 interface OnlineSummaryGraphUseCase {
 
