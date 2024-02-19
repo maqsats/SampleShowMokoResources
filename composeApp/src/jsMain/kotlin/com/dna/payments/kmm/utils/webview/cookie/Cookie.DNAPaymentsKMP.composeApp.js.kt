6444ab07012291e8973b9f9e322c8ba92e5cc252
Maksat Inkar <maksat.inkar.2003@gmail.com>
2024-02-19 18:20:47 +0600

    add actual class for web module

---
+++ composeApp/src/jsMain/kotlin/com/dna/payments/kmm/utils/webview/cookie/Cookie.DNAPaymentsKMP.composeApp.js.kt
@@ -0,0 +1,10 @@
+package com.dna.payments.kmm.utils.webview.cookie
+
+/**
+ * Get cookie expiration date.
+ * @param expiresDate The cookie expiration date in milliseconds.
+ * @return The cookie expiration date in [String] format.
+ * */
+actual fun getCookieExpirationDate(expiresDate: Long): String {
+    TODO("Not yet implemented")
+}
---
+++ composeApp/src/jsMain/kotlin/com/dna/payments/kmm/utils/webview/cookie/CookieManager.DNAPaymentsKMP.composeApp.js.kt
@@ -0,0 +1,9 @@
+package com.dna.payments.kmm.utils.webview.cookie
+
+/**
+ * Creates a [CookieManager] instance.
+ */
+@Suppress("FunctionName")
+actual fun WebViewCookieManager(): CookieManager {
+    TODO("Not yet implemented")
+}
